80c38cd61732265e2d8b718f159f7385b3ce0cd8
Tanner <thetannerelston@gmail.com>
2025-12-06 19:18:20 -0600

    Pushing 12/6/2025

---
+++ module-10/Elston-Module10_2/Elston-ToDo.py
@@ -0,0 +1,167 @@
+# Tanner Elston, 12/4/2025, Tkinter To-Do Assignment 10.2
+# (Love, github, 2018)
+# https://github.com/Dvlv/Tkinter-By-Example/blob/master/Tkinter-By-Example.pdf (2.1)
+
+import tkinter as tk
+import tkinter.messagebox as msg
+import sys
+
+
+class Todo(tk.Tk):
+    def __init__(self, tasks=None):
+        super().__init__()
+
+        # -------------------------------------------------
+        # Decide which mouse button is "right click" based on OS
+        # macOS: Button-2, Windows/Linux: Button-3
+        # -------------------------------------------------
+        if sys.platform == "darwin":      # macOS
+            self.DELETE_BUTTON = "<Button-2>"
+        else:                             # Windows / Linux
+            self.DELETE_BUTTON = "<Button-3>"
+
+        # -------------------------------------------------
+        # Window title: LastName-ToDo
+        # -------------------------------------------------
+        self.title("Elston-ToDo")
+        self.geometry("300x400")
+
+        # -------------------------------------------------
+        # Menu bar: File -> Exit + colored menu items
+        # Two complementary colors: Blue & Orange
+        # -------------------------------------------------
+        menubar = tk.Menu(
+            self,
+            bg="blue",             # menu bar background
+            fg="white",            # menu bar text
+            activebackground="orange",
+            activeforeground="black"
+        )
+
+        file_menu = tk.Menu(
+            menubar,
+            tearoff=0,
+            bg="orange",           # dropdown background
+            fg="black",            # dropdown text
+            activebackground="blue",
+            activeforeground="white"
+        )
+        file_menu.add_command(label="Exit", command=self.destroy)
+        menubar.add_cascade(label="File", menu=file_menu)
+        self.config(menu=menubar)
+        # -------------------------------------------------
+
+        if not tasks:
+            self.tasks = []
+        else:
+            self.tasks = tasks
+
+        self.tasks_canvas = tk.Canvas(self)
+        self.tasks_frame = tk.Frame(self.tasks_canvas)
+        self.text_frame = tk.Frame(self)
+
+        self.scrollbar = tk.Scrollbar(
+            self.tasks_canvas,
+            orient="vertical",
+            command=self.tasks_canvas.yview
+        )
+
+        self.tasks_canvas.configure(yscrollcommand=self.scrollbar.set)
+
+        self.task_create = tk.Text(self.text_frame, height=3, bg="orange", fg="black")
+
+        self.tasks_canvas.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+        self.scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+
+        self.canvas_frame = self.tasks_canvas.create_window(
+            (0, 0), window=self.tasks_frame, anchor="n"
+        )
+
+        self.task_create.pack(side=tk.BOTTOM, fill=tk.X)
+        self.text_frame.pack(side=tk.BOTTOM, fill=tk.X)
+        self.task_create.focus_set()
+
+        # -------------------------------------------------
+        # Instruction label: tell user how to delete a task
+        # -------------------------------------------------
+        todo1 = tk.Label(
+            self.tasks_frame,
+            text="Items Added --- ** Right Click Item to Delete**",
+            bg="orange",
+            fg="black",
+            pady=10
+        )
+
+        todo1.bind(self.DELETE_BUTTON, self.remove_task)
+        # -------------------------------------------------
+
+        self.tasks.append(todo1)
+
+        for task in self.tasks:
+            task.pack(side=tk.TOP, fill=tk.X)
+
+        self.bind("<Return>", self.add_task)
+        self.bind("<Configure>", self.on_frame_configure)
+        self.bind_all("<MouseWheel>", self.mouse_scroll)
+        self.bind_all("<Button-4>", self.mouse_scroll)
+        self.bind_all("<Button-5>", self.mouse_scroll)
+        self.tasks_canvas.bind("<Configure>", self.task_width)
+
+        self.colour_schemes = [
+            {"bg": "orange", "fg": "black"},
+            {"bg": "blue", "fg": "white"}
+        ]
+
+    def add_task(self, event=None):
+        task_text = self.task_create.get(1.0, tk.END).strip()
+
+        if len(task_text) > 0:
+            new_task = tk.Label(self.tasks_frame, text=task_text, pady=10)
+
+            self.set_task_colour(len(self.tasks), new_task)
+
+            new_task.bind(self.DELETE_BUTTON, self.remove_task)
+
+            new_task.pack(side=tk.TOP, fill=tk.X)
+            self.tasks.append(new_task)
+
+        self.task_create.delete(1.0, tk.END)
+
+    def remove_task(self, event):
+        task = event.widget
+        if msg.askyesno("Really Delete?", "Delete " + task.cget("text") + "?"):
+            self.tasks.remove(event.widget)
+            event.widget.destroy()
+            self.recolour_tasks()
+
+    def recolour_tasks(self):
+        for index, task in enumerate(self.tasks):
+            self.set_task_colour(index, task)
+
+    def set_task_colour(self, position, task):
+        _, task_style_choice = divmod(position, 2)
+        my_scheme_choice = self.colour_schemes[task_style_choice]
+        task.configure(bg=my_scheme_choice["bg"])
+        task.configure(fg=my_scheme_choice["fg"])
+
+    def on_frame_configure(self, event=None):
+        self.tasks_canvas.configure(scrollregion=self.tasks_canvas.bbox("all"))
+
+    def task_width(self, event):
+        canvas_width = event.width
+        self.tasks_canvas.itemconfig(self.canvas_frame, width=canvas_width)
+
+    def mouse_scroll(self, event):
+        if event.delta:
+            self.tasks_canvas.yview_scroll(int(-1 * (event.delta / 120)), "units")
+        else:
+            if event.num == 5:
+                move = 1
+            else:
+                move = -1
+            self.tasks_canvas.yview_scroll(move, "units")
+
+
+if __name__ == "__main__":
+    todo = Todo()
+    todo.mainloop()
